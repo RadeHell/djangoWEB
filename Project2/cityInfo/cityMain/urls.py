735c39bef76d4a71c549e1194bdae6d3ec7b5817
Radek Kratochvil <r1_kratochvil@utb.cz>
2024-07-01 21:20:07 +0200

    Prepared views and urls

---
+++ Project2/cityInfo/cityMain/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('cityMain/', views.cityMain , name='cityMain'),
+    path('cityNews/', views.cityNews , name='cityNews'),
+    path('cityAdministration/', views.cityAdministration , name='cityAdministration'),
+    path('cityFacts/', views.cityFacts , name='cityFacts'),
+    path('cityContacts/', views.cityContacts , name='cityContacts'),
+   
+
+]
---
+++ Project2/cityInfo/cityMain/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.template import loader
+
+def cityMain(request):
+  template = loader.get_template('cityMain.html')
+  return HttpResponse(template.render())
+
+def cityNews(request):
+  template = loader.get_template('cityNews.html')
+  return HttpResponse(template.render())
+
+def cityAdministration(request):
+  template = loader.get_template('cityAdministration.html')
+  return HttpResponse(template.render())
+
+def cityFacts(request):
+  template = loader.get_template('cityFacts.html')
+  return HttpResponse(template.render())
+
+  def cityContacts(request):
+  template = loader.get_template('cityContacts.html')
+  return HttpResponse(template.render())
